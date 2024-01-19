141c792a5c09d9b626d9206e7c17a3b8a50a036b
budzs <budai9902@gmail.com>
2024-01-19 20:31:50 +0100

    Fix CombinedLoss class in losses.py

--- src/am4ip/losses.py
+++ src/am4ip/losses.py
@@ -20,14 +20,6 @@ class DiceLoss(_Loss):
 class CombinedLoss(_Loss):
     def __init__(self, eps=1e-8, weight=None):
         super(CombinedLoss, self).__init__()
-<<<<<<< HEAD
-        self.we
-        self.dice_loss = DiceLoss(eps)
-
-    def forward(self, inp: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-        target_one_hot = torch.nn.functional.one_hot(target, num_classes=inp.shape[1]).permute(0, 3, 1, 2).float()
-        bce_loss = F.binary_cross_entropy_with_logits(inp, target_one_hot)
-=======
         self.eps = eps
         self.weight = torch.tensor([1.0, 1.0, 2.0, 1.0, 2.0])
         self.dice_loss = DiceLoss(eps)
@@ -35,7 +27,6 @@ class CombinedLoss(_Loss):
     def forward(self, inp: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         target_one_hot = torch.nn.functional.one_hot(target, num_classes=inp.shape[1]).permute(0, 3, 1, 2).float()  # Convert target to one-hot encoding
         bce_loss = F.binary_cross_entropy_with_logits(inp, target_one_hot, weight=self.weight)
->>>>>>> c4b1675b55a3e2bde55395c60a6738949f767abe
         dice_loss = self.dice_loss(inp, target)
         # Combine the two loss functions
         return dice_loss + bce_loss
