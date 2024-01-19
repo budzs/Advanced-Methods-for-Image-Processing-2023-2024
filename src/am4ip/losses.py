c4b1675b55a3e2bde55395c60a6738949f767abe
budzs <budai9902@gmail.com>
2024-01-19 20:00:57 +0100

    Add CombinedLoss class and weight parameter

--- src/am4ip/losses.py
+++ src/am4ip/losses.py
@@ -2,6 +2,7 @@
 import torch
 # noinspection PyProtectedMember
 from torch.nn.modules.loss import _Loss
+import torch.nn.functional as F
 
 class DiceLoss(_Loss):
     def __init__(self, eps=1e-8):
@@ -16,19 +17,17 @@ class DiceLoss(_Loss):
         dice = (2. * intersection + self.eps) / (union + self.eps)
         return 1. - dice.mean()
     
-import torch.nn.functional as F
 
 class CombinedLoss(_Loss):
-    def __init__(self, eps=1e-5):
+    def __init__(self, eps=1e-8, weight=None):
         super(CombinedLoss, self).__init__()
+        self.eps = eps
+        self.weight = torch.tensor([1.0, 1.0, 2.0, 1.0, 2.0])
         self.dice_loss = DiceLoss(eps)
 
     def forward(self, inp: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-        # Extract the tensors from the OrderedDicts
-        inp = inp['out']  # replace 'out' with the actual key
-        target = target['target']  # replace 'target' with the actual key
-
-        target_one_hot = torch.nn.functional.one_hot(target, num_classes=inp.shape[1]).permute(0, 3, 1, 2).float()
-        bce_loss = F.binary_cross_entropy_with_logits(inp, target_one_hot)
+        target_one_hot = torch.nn.functional.one_hot(target, num_classes=inp.shape[1]).permute(0, 3, 1, 2).float()  # Convert target to one-hot encoding
+        bce_loss = F.binary_cross_entropy_with_logits(inp, target_one_hot, weight=self.weight)
         dice_loss = self.dice_loss(inp, target)
-        return bce_loss + dice_loss
+        # Combine the two loss functions
+        return dice_loss + bce_loss
