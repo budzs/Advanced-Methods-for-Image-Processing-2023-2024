c1cf611ca91f74b2136abb4fb411c280de21946b
budzs <budai9902@gmail.com>
2024-01-21 00:05:55 +0100

    unet

---
+++ script/unet_train.py
@@ -0,0 +1,129 @@
+import sys
+import torch
+from torchvision.transforms import Compose, PILToTensor
+from torch.utils.data import DataLoader
+sys.path.append('C:/Users/budai/Downloads/am4ip-lab3-master_my/am4ip-lab3-master/src')
+# sys.path.append('/net/cremi/zbudai/am4ip-lab3-master-1/src')
+import itertools
+
+from am4ip.dataset import CropSegmentationDataset
+from am4ip.models import UNet
+from am4ip.trainer import BaselineTrainer
+from am4ip.losses import CombinedLoss, DiceLoss
+from am4ip.metrics import EvaluateNetwork
+
+from torchvision.transforms import Resize
+# from torchvision.models.segmentation import deeplabv3_resnet50
+from torchvision.transforms import Normalize
+from torchvision import models 
+# import segmentation_models_pytorch as smp
+from torch import nn
+import logging
+import datetime
+
+# Get the current time and format it as a string
+now = datetime.datetime.now()
+filename = now.strftime('%Y-%m-%d_%H-%M-%S.log')
+
+# Set up logging
+logging.basicConfig(filename=filename, level=logging.INFO, format='%(message)s')
+logger = logging.getLogger()
+
+# ImageNet mean and standard deviation
+mean = [0.485, 0.456, 0.406]
+std = [0.229, 0.224, 0.225]
+
+img_size = 224
+
+transform = Compose([
+    PILToTensor(),
+    lambda x: x.float(),  # Convert tensor to float
+    Resize((img_size, img_size), antialias=True),  # Resize  
+    Normalize(mean, std),  # Normalize with ImageNet mean and std
+    lambda z: z.to(dtype=torch.float32)
+])
+
+target_transform = Compose([
+    PILToTensor(),
+    Resize((img_size, img_size), antialias=True),  # Resize  
+    lambda z: z.to(dtype=torch.int64).squeeze(0)
+])
+
+val_transform = Compose([
+    PILToTensor(),
+    lambda x: x.float(),  # Convert tensor to float
+    Resize((img_size, img_size), antialias=True),  # Resize
+    Normalize(mean, std),  # Normalize with ImageNet mean and std
+    lambda z: z.to(dtype=torch.float32)
+])
+
+val_target_transform = Compose([
+    PILToTensor(),
+    Resize((img_size, img_size), antialias=True),  # Resize
+    lambda z: z.to(dtype=torch.int64).squeeze(0)
+])
+
+input_size = img_size*img_size*3
+
+dataset = CropSegmentationDataset(transform=transform, target_transform=target_transform, merge_small_items=True)
+val_dataset = CropSegmentationDataset(set_type="val", transform=val_transform, target_transform=val_target_transform, merge_small_items=True)
+# dataset.visualize_data()
+num_classes = dataset.get_class_number()
+print("Number of classes:", num_classes)
+# Calculate class counts
+# class_counts = [0] * dataset.get_class_number()
+# for _, target in dataset:
+#     for class_index in range(dataset.get_class_number()):
+#         class_counts[class_index] += (target == class_index).sum().item()
+# print("Class counts:", class_counts) 
+class_counts = [61674600, 8563795, 359237]
+
+# Define the hyperparameters
+batch_sizes = [16,32]
+epoch_sizes = [1, 10, 15, 20]
+learning_rates = [0.001, 0.0001, 0.01]
+
+# Create a list of all combinations of hyperparameters
+grid_list = list(itertools.product(batch_sizes, epoch_sizes, learning_rates))
+
+# Initialize variables to store the best parameters and the best score
+best_params = None
+best_score = float('-inf')
+
+# Loop over all combinations of hyperparameters
+logger.info("Pretraind model fcnresnet50, weights from class_counts, Dice")
+for params in grid_list:
+    logger.info(f"Parameters: batch_size={params[0]}, epoch={params[1]}, lr={params[2]}")
+    batch_size, epoch, lr = params
+
+    # Create the data loaders
+    train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+    # Create the model, loss function, and optimizer
+    # def __init__(self,in_channel,  n_classes, batchnorm=False,  bilinear=False):
+    unet = UNet(in_channel=3, n_classes=num_classes)  # Create the U-Net model
+    loss = CombinedLoss(class_counts=class_counts)
+    optimizer = torch.optim.SGD(unet.parameters(), lr=lr, momentum=0.8)
+
+    # Train the model
+    trainer = BaselineTrainer(model=unet, loss=loss, optimizer=optimizer, use_cuda=False)  # Use the U-Net model
+    trainer.fit(train_loader, val_data_loader=val_loader, epoch=epoch)
+
+    # Evaluate the model
+    unet_results, class_wise_results = EvaluateNetwork(unet, val_loader)  # Use the U-Net model
+
+    # If the current score is better than the best score, update the best score and the best parameters
+    if unet_results > best_score:
+        best_score = unet_results
+        best_params = params
+    # Log the results
+    logger.info(f"Score: {unet_results}")
+    logger.info(f"Class-wise results: ")
+    for i, result in enumerate(class_wise_results):
+        logger.info(f"Class {i}: {result}")
+logger.info(f"Best parameters: {best_params}")
+logger.info(f"Best score: {best_score}")
+
+print("Best parameters:", best_params)
+print("Best score:", best_score)
--- src/am4ip/models.py
+++ src/am4ip/models.py
@@ -1,102 +1,80 @@
 import torch
-from torch import nn, relu
+import torch.nn.functional as F
+import torch.nn as nn
+from collections import OrderedDict
 
-class UNet(nn.Module):
-    def __init__(self, n_class):
+class Block(nn.Module):
+    def __init__(self, ch_in, ch_out, bn):
         super().__init__()
-        
-        # Encoder
-        # In the encoder, convolutional layers with the Conv2d function are used to extract features from the input image. 
-        # Each block in the encoder consists of two convolutional layers followed by a max-pooling layer, with the exception of the last block which does not include a max-pooling layer.
-        # -------
-        # input: 572x572x3
-        self.e11 = nn.Conv2d(3, 64, kernel_size=3, padding=1) # output: 570x570x64
-        self.e12 = nn.Conv2d(64, 64, kernel_size=3, padding=1) # output: 568x568x64
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 284x284x64
-
-        # input: 284x284x64
-        self.e21 = nn.Conv2d(64, 128, kernel_size=3, padding=1) # output: 282x282x128
-        self.e22 = nn.Conv2d(128, 128, kernel_size=3, padding=1) # output: 280x280x128
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 140x140x128
-
-        # input: 140x140x128
-        self.e31 = nn.Conv2d(128, 256, kernel_size=3, padding=1) # output: 138x138x256
-        self.e32 = nn.Conv2d(256, 256, kernel_size=3, padding=1) # output: 136x136x256
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 68x68x256
-
-        # input: 68x68x256
-        self.e41 = nn.Conv2d(256, 512, kernel_size=3, padding=1) # output: 66x66x512
-        self.e42 = nn.Conv2d(512, 512, kernel_size=3, padding=1) # output: 64x64x512
-        self.pool4 = nn.MaxPool2d(kernel_size=2, stride=2) # output: 32x32x512
-
-        # input: 32x32x512
-        self.e51 = nn.Conv2d(512, 1024, kernel_size=3, padding=1) # output: 30x30x1024
-        self.e52 = nn.Conv2d(1024, 1024, kernel_size=3, padding=1) # output: 28x28x1024
-
-        # Decoder
-        self.upconv1 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
-        self.d11 = nn.Conv2d(1024, 512, kernel_size=3, padding=1)
-        self.d12 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-
-        self.upconv2 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.d21 = nn.Conv2d(512, 256, kernel_size=3, padding=1)
-        self.d22 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-
-        self.upconv3 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.d31 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
-        self.d32 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-
-        self.upconv4 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.d41 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
-        self.d42 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-
-        # Output layer
-        self.outconv = nn.Conv2d(64, n_class, kernel_size=1)
-
+        self.conv = nn.Sequential(
+            nn.Conv2d(ch_in, ch_out, 3, padding="same"),
+            nn.BatchNorm2d(ch_out) if bn else nn.Identity(),
+            nn.ReLU(inplace=True),
+        )
 
     def forward(self, x):
-        # Encoder
-        xe11 = relu(self.e11(x))
-        xe12 = relu(self.e12(xe11))
-        xp1 = self.pool1(xe12)
+        return self.conv(x)
 
-        xe21 = relu(self.e21(xp1))
-        xe22 = relu(self.e22(xe21))
-        xp2 = self.pool2(xe22)
 
-        xe31 = relu(self.e31(xp2))
-        xe32 = relu(self.e32(xe31))
-        xp3 = self.pool3(xe32)
+class DownSample(nn.Module):
+    def __init__(self, ch_in, ch_out, bn):
+        super().__init__()
+        self.block = nn.Sequential(
+            Block(ch_in, ch_out, bn),
+            Block(ch_out, ch_out, bn))
+        self.down_sample = nn.MaxPool2d(2)
 
-        xe41 = relu(self.e41(xp3))
-        xe42 = relu(self.e42(xe41))
-        xp4 = self.pool4(xe42)
+    def forward(self, x):
+        skip_out = self.block(x)
+        down_out = self.down_sample(skip_out)
+        return down_out, skip_out
 
-        xe51 = relu(self.e51(xp4))
-        xe52 = relu(self.e52(xe51))
+
+class UpSample(nn.Module):
+    def __init__(self, ch_in, ch_out, bn, bilinear):
+        super().__init__()
+        self.up_sample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True) if bilinear else nn.ConvTranspose2d(ch_in-ch_out, ch_in-ch_out, kernel_size=2, stride=2)
+        self.block = nn.Sequential(
+            Block(ch_in, ch_out, bn),
+            Block(ch_out, ch_out, bn))
+
+    def forward(self, down_input, skip_input):
+        x = self.up_sample(down_input)
+        x = torch.cat([x, skip_input], dim=1)
+        return self.block(x)
+
+
+class UNet(nn.Module):
+    def __init__(self,in_channel,  n_classes, batchnorm=False,  bilinear=False):
+        super(UNet, self).__init__()
+
+        self.down_conv1 = DownSample(in_channel, 64, batchnorm)
+        self.down_conv2 = DownSample(64, 128, batchnorm)
+        self.down_conv3 = DownSample(128, 256, batchnorm)
+        self.down_conv4 = DownSample(256, 512, batchnorm)
+
+        # Bottleneck
+        self.double_conv = nn.Sequential(
+                Block(512, 1024, batchnorm),
+                Block(1024, 1024, batchnorm))
         
-          # Decoder
-        xu1 = self.upconv1(xe52)
-        xu11 = torch.cat([xu1, xe42], dim=1)
-        xd11 = relu(self.d11(xu11))
-        xd12 = relu(self.d12(xd11))
+        self.up_conv4 = UpSample(512 + 1024, 512, batchnorm, bilinear)
+        self.up_conv3 = UpSample(256 + 512, 256, batchnorm, bilinear)
+        self.up_conv2 = UpSample(128 + 256, 128, batchnorm, bilinear)
+        self.up_conv1 = UpSample(128 + 64, 64, batchnorm, bilinear)
+        
+        self.out = nn.Conv2d(64, n_classes, kernel_size=1)
 
-        xu2 = self.upconv2(xd12)
-        xu22 = torch.cat([xu2, xe32], dim=1)
-        xd21 = relu(self.d21(xu22))
-        xd22 = relu(self.d22(xd21))
-
-        xu3 = self.upconv3(xd22)
-        xu33 = torch.cat([xu3, xe22], dim=1)
-        xd31 = relu(self.d31(xu33))
-        xd32 = relu(self.d32(xd31))
-
-        xu4 = self.upconv4(xd32)
-        xu44 = torch.cat([xu4, xe12], dim=1)
-        xd41 = relu(self.d41(xu44))
-        xd42 = relu(self.d42(xd41))
-
-        # Output layer
-        out = self.outconv(xd42)
-
-        return out
+    def forward(self, x):
+        x, skip1_out = self.down_conv1(x)
+        x, skip2_out = self.down_conv2(x)
+        x, skip3_out = self.down_conv3(x)
+        x, skip4_out = self.down_conv4(x)
+        x = self.double_conv(x)
+        x = self.up_conv4(x, skip4_out)
+        x = self.up_conv3(x, skip3_out)
+        x = self.up_conv2(x, skip2_out)
+        x = self.up_conv1(x, skip1_out)
+        x = self.out(x)
+        return x
+    
